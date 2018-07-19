dce761168c8e4aec40802d188a80b949f0cc5c74
Samuel McHale <sam@samuels-mbp-2.home>
2018-07-19 22:17:16 +0100

    This file contains classes for aliens, bullets and your ship to be implemented in the logic file

---
+++ game_objects.py
@@ -0,0 +1,36 @@
+#Space Invaders Game objects - Samuel McHale - 19/7/18
+
+class alien():
+    
+    def __init__(self, pointScore, shape):
+        self.pointScore = pointScore
+        self.properties = shape
+        self.ob = 'alien'
+
+    def drift(self):
+        self.properties[1] += 1
+
+class player():
+    
+    def __init__(self, shape):
+        self.properties = shape
+        self.ob = 'user'
+    
+    def move(self,key):
+        if key == 275:
+            if (self.properties[0] + 10) <= 650:
+                self.properties[0] += 10
+        elif key == 276:
+            if (self.properties[0] - 10) >= 0:
+                self.properties[0] -= 10
+
+class bullet():
+
+    def __init__(self, pos):
+        self.properties = [pos[0],pos[1],5,10]
+        self.ob = 'bullet'
+
+    def drift(self):
+        self.properties[1] -= 3
+
+
